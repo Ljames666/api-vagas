2661a0acd196223461623227fae61a1de0855e14
Roger Medeiros <roger@roger.dev.br>
2023-03-21 20:04:22 -0300

    atualizar usuario

--- src/app/features/usuario/repositories/usuario.repository.ts
+++ src/app/features/usuario/repositories/usuario.repository.ts
@@ -1,53 +1,58 @@
-import { DataBaseConnection } from '../../../../main/database/typeorm.connection';
-import { User } from '../../../models/user.model';
-import { UserEntity } from '../../../shared/entities/user.entity';
+import { DataBaseConnection } from "../../../../main/database/typeorm.connection";
+import { User } from "../../../models/user.model";
+import { UserEntity } from "../../../shared/entities/user.entity";
 
 export class UsuarioRepository {
-    private _repository = DataBaseConnection.connection.getRepository(UserEntity);
+  private _repository = DataBaseConnection.connection.getRepository(UserEntity);
 
-    public async criaUsuario(user: User) {
-        const usuarioEntidade = this._repository.create({
-            id: user.id,
-            nome: user.nome,
-            nomeEmpresa: user.nomeEmpresa,
-            senha: user.senha,
-            username: user.username,
-            tipo: user.tipo,
-        });
-        const result = await this._repository.save(usuarioEntidade);
+  public async criaUsuario(user: User) {
+    const usuarioEntidade = this._repository.create({
+      id: user.id,
+      nome: user.nome,
+      nomeEmpresa: user.nomeEmpresa,
+      senha: user.senha,
+      username: user.username,
+      tipo: user.tipo,
+    });
+    const result = await this._repository.save(usuarioEntidade);
 
-        return this.mapEntityToModel(result);
+    return this.mapEntityToModel(result);
+  }
+
+  public async atualizar(nome: string, senha: string) {
+    const result = await this._repository.findOneBy({
+      nome,
+    });
+
+    if (senha) {
+      result!.senha = senha;
     }
 
-    public async obtemPorUserName(username: string) {
-        const result = await this._repository.findOneBy({
-            username,
-        });
+    await this._repository.save(result!);
+  }
 
-        if (!result) {
-            return null;
-        }
+  public async obtemPorUserName(username: string) {
+    const result = await this._repository.findOneBy({
+      username,
+    });
 
-        return this.mapEntityToModel(result);
-    }
-    public async getAll() {
-        const result = await this._repository.find();
-
-        if (!result) {
-            return null;
-        }
-
-        return result.map((user) => this.mapEntityToModel(user));
+    if (!result) {
+      return null;
     }
 
-    private mapEntityToModel(entity: UserEntity) {
-        return User.create(
-            entity.id,
-            entity.nome,
-            entity.username,
-            entity.senha,
-            entity.tipo,
-            entity.nomeEmpresa
-        );
+    return this.mapEntityToModel(result);
+  }
+  public async getAll() {
+    const result = await this._repository.find();
+
+    if (!result) {
+      return null;
     }
+
+    return result.map((user) => this.mapEntityToModel(user));
+  }
+
+  private mapEntityToModel(entity: UserEntity) {
+    return User.create(entity.id, entity.nome, entity.username, entity.senha, entity.tipo, entity.nomeEmpresa);
+  }
 }
